8f4021593ff31143abb9caa08eec5822191128ce
Macgithub13 <macgames444@gmail.com>
2023-11-17 17:12:17 -0300

    teste

--- API/src/repository/connection.js
+++ API/src/repository/connection.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
 
-    host:process.env.MYSQL_HOST,
-    user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DATABASE
+    MYSQL_host:process.env.MYSQL_HOST,
+    MYSQL_user:process.env.MYSQL_USER,
+    MYSQL_password:process.env.MYSQL_PASSWORD,
+    MYSQL_database:process.env.MYSQL_DATABASE
 });
 
 console.log('BD Conectado');
