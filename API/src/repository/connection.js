7e8f812238e5522c11c749cb2f36756be0c0ed82
Macgithub13 <macgames444@gmail.com>
2023-11-17 17:14:21 -0300

    outro erro

--- API/src/repository/connection.js
+++ API/src/repository/connection.js
@@ -4,7 +4,7 @@ const connection = await mysql.createConnection({
 
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASSWORD,
+    password:process.env.MYSQL_PWD,
     database:process.env.MYSQL_DATABASE
 });
 
